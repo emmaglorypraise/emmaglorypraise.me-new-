5802088d4f132ef4e64b32bc46f302066a8f013b
emmaglorypraise <emmaglorypraise@gmail.com>
2020-04-19 22:11:26 +0100

    updated features
    added download  resume option and updateted features

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -92,7 +92,7 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     var action = $(this).attr('action');
     if( ! action ) {
-      action = 'contactform/contactform.php';
+      action = 'https://formspree.io/emmaglorypraise.gmail.com';
     }
     $.ajax({
       type: "POST",
